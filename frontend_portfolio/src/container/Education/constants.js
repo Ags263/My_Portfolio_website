6967175cbd5f749550e199d59cd9aef74c9788ca
Ashish Behera <141529216+Ags263@users.noreply.github.com>
2024-11-02 19:32:45 +0530

    Add files via upload

---
+++ frontend_portfolio/src/container/Education/constants.js
@@ -0,0 +1,19 @@
+  export const education = [{
+          id: 0,
+          img: require('./mcc.png'), // Update this path
+          school: "Mulund College of Commerce, Mumbai",
+          date: "Oct 2021 - Apr 2024",
+          grade: "8.8 CGPA",
+          desc: "I have  completed my Bachelor's degree in Computer Science and Graduation at Mulund College of Commerce, Mumbai. I have completed 5 semesters and have a CGPA of 7.8. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks,And able to learn about New technology and networking with people.",
+          degree: "Bachelor of Computer Science",
+      },
+      {
+          id: 1,
+          img: require('./image.png'), // Update this path
+          school: "St Xaviers Junior College, Mumbai",
+          date: "Apr 2019 - Apr 2021",
+          grade: "77.17%",
+          desc: "I completed my class 12 high school education at St XaviersJunior College, Mumbai, where I studied Science.",
+          degree: "(XII), Science",
+      },
+  ];
