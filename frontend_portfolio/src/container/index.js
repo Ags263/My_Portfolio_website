0fb34125609bf32da45e27c7a3ead56049aa7740
Ashish Behera <141529216+Ags263@users.noreply.github.com>
2024-11-02 19:31:53 +0530

    Create index.js

---
+++ frontend_portfolio/src/container/index.js
@@ -0,0 +1,15 @@
+import About from './About/About';
+import Header from './Header/Header';
+import Skills from './Skills/Skills';
+import Education from './Education/Education';
+import Work from './Work/Work';
+import Contact from './Contact/Contact';
+
+export {
+  About,
+  Header,
+  Skills,
+  Education,
+  Work,
+  Contact,
+};
