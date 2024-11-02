31d01dc3a1bab82ec37f3e6017b447cc8abc6ff7
Ashish Behera <141529216+Ags263@users.noreply.github.com>
2024-11-02 19:10:26 +0530

    Add files via upload

---
+++ backend_sanity/schemaTypes/index.js
@@ -0,0 +1,15 @@
+export const schemaTypes = [ works,
+    aboutme,
+    abouts,
+    skills,
+    workExperience,
+    experiences,
+    contact,]
+
+    import works from "./works";
+    import abouts from "./abouts";
+    import experiences from "./experiences";
+    import skills from "./skills";
+    import workExperience from "./workExperience";
+    import contact from "./contact";
+    import aboutme from "./aboutme";
