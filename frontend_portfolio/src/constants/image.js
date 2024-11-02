f1a02fad5e3fbbb9f9509081e1a9b2d8544f204e
Ashish Behera <141529216+Ags263@users.noreply.github.com>
2024-11-02 19:29:40 +0530

    Create image.js

---
+++ frontend_portfolio/src/constants/image.js
@@ -0,0 +1,54 @@
+import email from '../assets/email.png';
+import cpp from '../assets/cpp.png';
+import css from '../assets/css.png';
+import figma from '../assets/figma.png';
+import flutter from '../assets/flutter.png';
+import git from '../assets/git.png';
+import graphql from '../assets/graphql.png';
+import html from '../assets/html.png';
+import javascript from '../assets/javascript.png';
+import mu5 from '../assets/mu5.png';
+import node from '../assets/node.png';
+import python from '../assets/python.png';
+import react from '../assets/react.png';
+import redux from '../assets/redux.png';
+import sass from '../assets/sass.png';
+
+
+
+import profile from '../assets/profile.png';
+import circle from '../assets/circle.svg';
+import logo from '../assets/logo.png';
+
+import adidas from '../assets/adidas.png';
+import amazon from '../assets/amazon.png';
+import asus from '../assets/asus.png';
+import bolt from '../assets/bolt.png';
+import nb from '../assets/nb.png';
+import spring from '../assets/spring.png';
+import java from '../assets/java.png';
+
+export default {
+  java,
+  email,
+  mobile,
+  api,
+  cpp,
+  css,
+  figma,
+  flutter,
+  graphql,
+  html,
+  javascript,
+  node,
+  react,
+  redux,
+  sass,
+  profile,
+  circle,
+  logo,
+  adidas,
+  amazon,
+  nb,
+  spring,
+};
